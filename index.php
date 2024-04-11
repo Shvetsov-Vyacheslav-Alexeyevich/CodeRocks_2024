51567ccb725ecd25a963c41d9f03bbf2e483ef4d
NETEG0N <kostyankozlov2309@gmail.com>
2024-04-12 01:45:35 +0700

    qwer
    wsdf

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/header.php");
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/products.php");
-  require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/edit_product.php");
+  require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/edit_name_company.php");
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/footer.php");
 ?>
