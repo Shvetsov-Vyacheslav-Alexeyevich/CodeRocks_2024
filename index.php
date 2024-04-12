43152ed4522189bdb5c0e5e84936c109df634d37
shvetsov-vyacheslav-alexeyevich <shvetsov-tech@yandex.ru>
2024-04-12 23:41:39 +0700

    asdsad
    sdasdasda

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
   session_start();
-
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/header.php");
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/products.php");
-  require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/add_punct.php");
+  require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/add_stock.php");
   require($_SERVER["DOCUMENT_ROOT"] . "/sources/blocks/footer.php");
 ?>
